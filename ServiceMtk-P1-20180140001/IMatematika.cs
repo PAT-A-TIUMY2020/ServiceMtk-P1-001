84378c5c32f5def3d5b280f1459b1801bdf88ee2
yusufhardi10 <yusufhardi10@gmail.com>
2020-11-21 04:35:53 +0700

    Add WCF Exception Handling

--- ServiceMtk-P1-20180140001/IMatematika.cs
+++ ServiceMtk-P1-20180140001/IMatematika.cs
@@ -13,32 +13,50 @@ namespace ServiceMtk_P1_20180140001
     {
         [OperationContract]
         /* output->*/
+        [FaultContract(typeof(MathFault))]
         int Tambah(int a, int b); //method
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kurang(int a, int b); //input
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kali(int a, int b);
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Bagi(int a, int b);
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class
 
-        [DataContract]
-        public class Koordinat
+        // TODO: Add your service operations here
+    }
+    [DataContract]
+    class MathFault
+    {
+        [DataMember]
+        public string Kode { get; set; }
+        [DataMember]
+        public string Pesan { get; set; }
+    }
+    public class Koordinat
+    {
+        private int _x, _y; //atribut
+        [DataMember]//membuka koordinat, x,y
+        public int X //properties
         {
-            private int _x, _y; //atribut
-            [DataMember]//membuka koordinat, x,y
-            public int X //properties
-            {
-                get { return _x; }
-                set { _x = value; }
-            }
-            [DataMember]
-            public int Y //properties
-            {
-                get { return _y; }
-                set { _y = value; }
-            }
+            get { return _x; }
+            set { _x = value; }
         }
+        [DataMember]
+        public int Y //properties
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
     }
 }
